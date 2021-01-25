2bfecf3e180bc2357b1ad9a8a6940d7bf56619c3
Ib Helmer Nielsen <ihn@ihn.dk>
2021-01-25 23:29:41 +0100

    Update

--- leakmon.py
+++ leakmon.py
@@ -1,6 +1,7 @@
 import csv
 import time
 from w1thermsensor import W1ThermSensor
+
 with open('measurement.csv', 'w', newline='') as file:
     start = time.time()
     writer = csv.writer(file)
@@ -10,5 +11,5 @@ with open('measurement.csv', 'w', newline='') as file:
         for sensor in W1ThermSensor.get_available_sensors():
             mess.append(sensor.get_temperature())
         writer.writerow([str(round(time.time()-start,2)),str(mess[0]) ,str(mess[1]), str(mess[0]-mess[1])])
-        time.sleep(8.2)
-        print(i)
+        time.sleep(8.2)		# 10 sec - measurement time
+        print(i)		# Print mesurement count
